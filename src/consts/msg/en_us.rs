50fe20a5aa666040409b873ea93cd91aecda8164
u007d <b2b@humanenginuity.com>
2020-03-20 20:50:20 -0700

    arith_traits initial commit

---
+++ src/consts/msg/en_us.rs
@@ -0,0 +1,3 @@
+#![allow(dead_code)]
+pub const ERR_INVALID_INT_REPR: &str = "Error: String contents are not a valid representation of \
+                                        an integer";
---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+#![warn(clippy::all, clippy::nursery, clippy::pedantic, rust_2018_idioms)]
+#![forbid(bare_trait_objects)]
+#![allow(clippy::match_bool)]
+// To use the `unsafe` keyword, change to `#![allow(unsafe_code)]` (do not remove); aids auditing.
+#![forbid(unsafe_code)]
+// Safety-critical application lints
+#![deny(
+    clippy::pedantic,
+    clippy::float_cmp_const,
+    clippy::indexing_slicing,
+    clippy::integer_arithmetic,
+    clippy::option_unwrap_used,
+    clippy::result_unwrap_used
+)]
+
+// Uncomment before ship to reconcile use of possibly redundant crates, debug remnants, missing license files and more
+//#![warn(clippy::cargo, clippy::restriction, missing_docs, warnings)]
+//#![deny(warnings)]
+
+mod consts;
+mod error;
+pub use error::Error;
+pub type Result<T, E = Error> = std::result::Result<T, E>;
