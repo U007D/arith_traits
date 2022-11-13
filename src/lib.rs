1588f2d0e856cbaf70e38c0c50bbc7b77b6b7f28
Brad Gibson <b2b@humanenginuity.com>
2022-11-13 08:27:18 -0800

    remove policy types (migrate them to arithmetic policy crate)

--- src/lib.rs
+++ src/lib.rs
@@ -32,10 +32,8 @@
 
 #[macro_use]
 mod op_impl_macros; // macros must be declared before trait modules
-mod policy;
 mod traits;
 #[cfg(feature = "xl_types")]
 mod xl_types;
 
-pub use policy::{OverflowingPolicy, WrappingPolicy};
 pub use traits::{ICheckedOps, IMinMax, IOverflowingOps, ISaturatingOps, IWrappingOps};
--- src/policy.rs
+++
@@ -1,5 +0,0 @@
-mod overflowing;
-mod wrapping;
-
-pub use overflowing::OverflowingPolicy;
-pub use wrapping::WrappingPolicy;
